3aa95bd13d400a34cd3141cfdb9d0947f414e2d5
asgerkrabbe <Asgerkrabbe@gmail.com>
2021-09-08 15:19:01 +0200

    Initial commit

---
+++ src/com/kea/Main.java
@@ -0,0 +1,11 @@
+package com.kea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+    }
+}
---
+++ src/com/kea/OrbitalObject.java
@@ -0,0 +1,6 @@
+package com.kea;
+
+public abstract class OrbitalObject {
+
+
+}
